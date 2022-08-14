d1569e9556c3922f9d37234a82edb7840ec3af81
Manish Sharma <code.manish94@gmail.com>
2022-08-14 13:06:15 +0530

    added modifier
    refactor

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,7 +4,7 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  int levelUpFee = 0.001 ether;
+  uint levelUpFee = 0.001 ether;
 
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
@@ -12,11 +12,11 @@ contract ZombieHelper is ZombieFeeding {
   }
 
   function withdraw() external onlyOwner {
-    address payable _owner = address(uint160(owner()));
+    address _owner = owner();
     _owner.transfer(address(this).balance);
   }
 
-  function setLevelUpFee(uint _fee) external onlyOwner{
+  function setLevelUpFee(uint _fee) external onlyOwner {
     levelUpFee = _fee;
   }
 
@@ -25,13 +25,11 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].level++;
   }
 
-  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
     zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
     zombies[_zombieId].dna = _newDna;
   }
 
@@ -46,4 +44,5 @@ contract ZombieHelper is ZombieFeeding {
     }
     return result;
   }
+
 }
